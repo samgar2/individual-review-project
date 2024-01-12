bbed1f7a7c7406cd5aec6040277ce18e3eab83a2
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-12 12:50:27 -0800

    seedData updates

--- server/seedData.js
+++ server/seedData.js
@@ -2,11 +2,11 @@
 
 //characters
 const characters = [
-    {name: "Jane Bennet", description: "#", quote: "#", image: "#"},
-    {name: "Elizabeth Bennet", description: "#", quote: "#", image: "#"},
-    {name: "Elinor Dashwood", description: "#", quote: "#", image: "#"},
-    {name: "Marianne Dashwood", description: "#", quote: "#", image: "#"},
-    {name: "Emma Woodhouse", description: "#", quote: "#", image: "#"}
+    {name: "Jane Bennet", bookId: 1, description: "#", quote: "#", image: "#"},
+    {name: "Elizabeth Bennet", bookId: 1, description: "#", quote: "#", image: "#"},
+    {name: "Elinor Dashwood", bookId: 2, description: "#", quote: "#", image: "#"},
+    {name: "Marianne Dashwood", bookId: 2, description: "#", quote: "#", image: "#"},
+    {name: "Emma Woodhouse", bookId: 3, description: "#", quote: "#", image: "#"}
 ]
 
 //books
@@ -18,16 +18,16 @@ const books = [
 
 //outfits
 const outfits = [
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"},
-    {name: "#", character: "#", book: "#", image: "#"}
+    {name: "#", characterId: 1, bookId: 1, image: "#"},
+    {name: "#", characterId: 1, bookId: 1, image: "#"},
+    {name: "#", characterId: 2, bookId: 2, image: "#"},
+    {name: "#", characterId: 2, bookId: 2, image: "#"},
+    {name: "#", characterId: 3, bookId: 3, image: "#"},
+    {name: "#", characterId: 3, bookId: 3, image: "#"},
+    {name: "#", characterId: 4, bookId: 4, image: "#"},
+    {name: "#", characterId: 4, bookId: 4, image: "#"},
+    {name: "#", characterId: 5, bookId: 5, image: "#"},
+    {name: "#", characterId: 5, bookId: 5, image: "#"}
 ]
 
 //export our mock data variables for use elsewhere
