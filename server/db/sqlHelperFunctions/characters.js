e662e34c532c1e5d3cbc011084e9a42ddefc60c1
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-16 20:07:57 -0800

    sql helper functions work

--- server/db/sqlHelperFunctions/characters.js
+++ server/db/sqlHelperFunctions/characters.js
@@ -16,5 +16,66 @@ async function getAllCharacters() {
     }
 }
 
+// GET: show a single character
+async function getCharacterById(id) {
+    try {
+        const { rows: [character] } = await client.query(`
+            SELECT * FROM characters
+            WHERE id = $1;
+        `, [id]);
+        return character;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// POST: create a new character
+async function createCharacter(body) {
+    const { name, bookId, description, quote, image } = body;
+    try {
+        const { rows: [character] } = await client.query(`
+            INSERT INTO characters(name, "bookId", description, quote, image)
+            VALUES($1, $2, $3, $4, $5)
+            RETURNING *;
+        `, [name, bookId, description, quote, image]);
+        return character;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// PUT: update a single character
+async function updateCharacter(id, fields = {}) {
+    const setString = Object.keys(fields).map((key, index) => `"${key}"=$${index + 1}`).join(', ');
+    if (setString.length === 0) {
+        return;
+    }
+    try {
+        const { rows: [character] } = await client.query(`
+            UPDATE characters
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+        `, Object.values(fields));
+        return character;
+    } catch (error) {
+        throw error;
+    }
+}
+
+// DELETE: delete a single character
+async function deleteCharacter(id) {
+    try {
+        const { rows: [character] } = await client.query(`
+            DELETE FROM characters
+            WHERE id=$1
+            RETURNING *;
+        `, [id]);
+        return character;
+    } catch (error) {
+        throw error;
+    }
+}
+
 //export our funcions
-module.exports = { getAllCharacters }
+module.exports = { getAllCharacters, getCharacterById, createCharacter, updateCharacter, deleteCharacter }
