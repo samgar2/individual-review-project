8a97b8eba9858126fc88e5e9e838d0847e1acf4f
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-18 13:39:10 -0800

    setting up react and redux files and folders for frontend work

---
+++ client/src/redux/janeAustenApi.js
@@ -0,0 +1,3 @@
+//put all of my fetch calls in here: have a corresponding fetch request for each API route (according to directions)
+
+//bookBuddyApi.js file and the store.js file for redux instead of having a fetching.js file
