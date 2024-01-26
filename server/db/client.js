f44beb09030c9b8aa48183042e41b44976cfdec0
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-26 13:51:20 -0800

    adding render url to backend for deployment

--- server/db/client.js
+++ server/db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg')
 
 //Establishing the connection to the database (like how we do with http://)
-const client = new Client(`postgres://janeausten_user:9O6svYjpUU8YojAa4T4PGuBagQc8dZGU@dpg-cmq1m5qcn0vc73dl9fd0-a/janeausten`)
+const client = new Client(`postgres://janeausten_4ccm_user:f3ZO6blhctHjFPymnekbD8IP8UQeiI8G@dpg-cmq2i2821fec739jiblg-a/janeausten_4ccm`)
 
 //export for use in other files
 module.exports = client
