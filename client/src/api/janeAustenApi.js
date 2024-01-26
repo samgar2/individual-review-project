5ae9698f0f7c4d0f15dbe2828e9f4558b87a5efd
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-26 13:59:13 -0800

    added url from render to front end

--- client/src/api/janeAustenApi.js
+++ client/src/api/janeAustenApi.js
@@ -6,7 +6,7 @@ export const janeAustenApi = createApi({
   reducerPath: "janeAustenApi",
   //Set the base URL for API requests
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api",
+    baseUrl: "http://jane-austen.onrender.com/api",
     //Set headers
     prepareHeaders(headers) {
       headers.set("Content-type", "application/json");
