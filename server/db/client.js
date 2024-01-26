7f7a303eb45292b0b48f5f804218de51406791fc
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-26 12:56:10 -0800

    working on backend deploying just updated the internal database url in my client js file to the one i made on render

--- server/db/client.js
+++ server/db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg')
 
 //Establishing the connection to the database (like how we do with http://)
-const client = new Client(`postgres://localhost:54321/janeausten`)
+const client = new Client(`postgres://janeausten_user:9O6svYjpUU8YojAa4T4PGuBagQc8dZGU@dpg-cmq1m5qcn0vc73dl9fd0-a/janeausten`)
 
 //export for use in other files
 module.exports = client
