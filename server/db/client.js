1e130ceed25f69f336d8012dbdbf2485a88ce5db
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2024-01-29 09:21:16 -0800

    deploying

--- server/db/client.js
+++ server/db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require('pg')
 
 //Establishing the connection to the database (like how we do with http://)
-const client = new Client(`postgres://localhost:54321/janeausten`)
+const client = new Client(`postgres://janeausten_6jlm_user:cG5Ue7tF2PiTt1jbB6jm4PjGbPZp6cdu@dpg-cmrtsd0cmk4c7383g2tg-a/janeausten_6jlm`)
 
 //export for use in other files
 module.exports = client
